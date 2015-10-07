40d8e3f499e9433b1bb1564528c57ccc987e3e3f
kaysush <kumar.sushil@outlook.com>
2015-10-07 22:30:42 +0530

    OAuth request token generation

--- application/Scripts/app.js
+++ application/Scripts/app.js
@@ -7,27 +7,10 @@ app.controller('MainController', function ($scope, $http) {
 
     $scope.getRequestToken = function () {
 
-        var config = {
-            url: 'https://secure.splitwise.com/api/v3.0/get_request_token',
-            method: 'POST',
-            data: {}
-        };
+          
 
 
-
-
-
-        var oauth = OAuth({
-            consumer: {
-                public: 'E9CXT5qfXEdcw93pjseyNZ1751Gr4vFNwRlOKDz7',
-                secret: 'b8ZvNVseethUYeA21ZWMT1EUfc7ZDuzdvBn4wLiR'
-            },
-            signature_method: 'HMAC-SHA1'
-        });
-
-
-        console.log(oauth.toHeader(oauth.authorize(config)));
-
+        
         $http({ url: config.url, method: config.method, headers: oauth.toHeader(oauth.authorize(config)) }).
         success(function (data) {
             console.log(data);
--- server.js
+++ server.js
@@ -1,10 +1,63 @@
 var express = require('express');
+var https = require('https');
+var OAuth   = require('oauth-1.0a');
+var queryString = require('query-string');
 
 var port = 8000;
-var host = 127.0.01;
+var host = '127.0.0.1';
+var publicKey = process.env.SPLITWISE_PUBLIC_KEY;
+var secretKey = process.env.SPLITWISE_SECRET_KEY;
+
+
 var app = express();
 
-express.use(express.static('application'));
+app.use(express.static('application'));
+
+app.get('/get_authentication_token',function(request,response){
+	
+	var config = {
+            url: 'https://secure.splitwise.com/api/v3.0/get_request_token',
+            method: 'POST',
+            data: {}
+        };
+
+
+    var oauth = OAuth({
+            consumer: {
+                public: publicKey,
+                secret: secretKey
+            },
+            signature_method: 'HMAC-SHA1'
+        });
+
+    var oauthHeaderObject = oauth.toHeader(oauth.authorize(config));
+
+    var httpOpts = {
+    	host : 'secure.splitwise.com',
+    	path:'/api/v3.0/get_request_token',
+    	method : config.method,
+    	headers : oauthHeaderObject
+    };
+    
+    var httpRequest = https.request(httpOpts , function(httpResponse){
+    	var data = '';
+    	
+    	httpResponse.on('data',function(chunk){
+    		
+    		data += chunk;
+    	});
+
+    	httpResponse.on('end',function(){
+    		var obj = queryString.parse(data);
+    		response.json(obj);
+    	});
+    });
+
+
+    httpRequest.end();
+    
+
+});
 
 
 app.listen(port,host,function(){
